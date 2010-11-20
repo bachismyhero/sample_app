2842c5bb45ed698421107bea8f84b0fa0ab15598
mmr <mmr@castalia.localdomain>
2010-11-20 11:58:39 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '9cbcb183f911b81dedbe3a4f6229b7c9d10a4f786d07bfbfcc749232a10e76adf8688a3c4d03d2b9b4a573ebbf6ece2d4f0b6e8ccf4a5c3b5fb422e662a5d6a9'
